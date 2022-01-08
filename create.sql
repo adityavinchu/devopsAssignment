d456b4d1d8a1aeab59875fe7c7876c41172e5c44
adityavinchu <88549097+adityavinchu@users.noreply.github.com>
2022-01-08 22:39:46 +0530

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,7 @@
+create table studentData(
+    name varchar(25),
+    roll_no varchar(10),
+    address text,
+    mobile_number varchar(10),
+    pan_number varchar(10)
+);
---
+++ insertData.sql
@@ -0,0 +1,4 @@
+insert into studentData values('Akshay','1','Pune','7888899888','10112144');
+insert into studentData values('Pratik','2','Mumbai','9888747488','12345679');
+insert into studentData values('Vikrant','3','Kanpur','9888557477','14147457');
+insert into studentData values('Bhushan','4','Nashik','8888888888','14574475');
